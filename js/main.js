4653f0193e381d54b6d89d11705cf5d90d2574a2
nnish16 <66774185+nnish16@users.noreply.github.com>
2022-01-23 19:46:19 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,85 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
+
+});
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset();
+        status.classList.add('success')
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form";
+        status.classList.add('error')
+    });
+}
+form.addEventListener("submit", handleSubmit)
+
+
+ var typed = new Typed(".typing-2", {
+        strings: ["Future Billionaires.","Crypto Traders.", " Intelligent Investors.", " Students." , "Crypto Critics.", "Researchers.", "Crypto Enthusiasts."],
+        typeSpeed: 100,
+        trackSpeed: 60,
+        backSpeed: 60,
+        loop: true
+
+    })
+
+
+
+
